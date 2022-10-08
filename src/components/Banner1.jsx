c405cdb4b67533bc2e366742ca9c9a7bdfcdbc51
Nikhil Tamboli <nikhiltamboli17@gmail.com>
2022-10-08 15:59:19 +0530

    checkpoint

---
+++ src/components/Banner1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Banner1 extends Component {
+    state = {  } 
+    render() { 
+        
+        return (
+            <div id='test'></div>
+        );
+    }
+}
+ 
+export default Banner1;
